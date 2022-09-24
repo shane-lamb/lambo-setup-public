086c985eeebb071ed5c9ad8ca72cf6b668ea4f1f
Shane Lamb <shanelamb1@gmail.com>
2022-09-24 12:09:00 +1000

    added to zsh setup (include alias file)

--- setup.sh
+++ setup.sh
@@ -18,3 +18,21 @@ if [[ -L ~/.zshrc ]]; then
 fi
 ln -s "$SCRIPT_DIR"/.zshrc ~/.zshrc
 
+if [[ -L ~/.zprofile ]]; then
+   rm ~/.zprofile
+fi
+ln -s "$SCRIPT_DIR"/.zprofile ~/.zprofile
+
+if [[ -L ~/.p10k.zsh ]]; then
+   rm ~/.p10k.zsh
+fi
+ln -s "$SCRIPT_DIR"/.p10k.zsh ~/.p10k.zsh
+
+if [[ ! -f "$SCRIPT_DIR"/private/alias.zsh ]]; then
+   mkdir -p "$SCRIPT_DIR"/private
+   touch "$SCRIPT_DIR"/private/alias.zsh
+fi
+if [[ -L ~/alias.zsh ]]; then
+   rm ~/alias.zsh
+fi
+ln -s "$SCRIPT_DIR"/private/alias.zsh ~/alias.zsh
