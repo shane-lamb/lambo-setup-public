4c170acb7bdfe397ad7ac864ceccfe6644909329
Shane Lamb <shanelamb1@gmail.com>
2025-07-19 12:37:57 +1000

    karabiner: pressing app shortcut while app is open will cycle windows

--- karabiner/generate.js
+++ karabiner/generate.js
@@ -1,4 +1,4 @@
-const semicolonKeyMappings = {
+const keyToManipulators = {
     '1': toSuper('1'), // for window resizing with rectangle
     '2': toSuper('2'), // for window resizing with rectangle
     '3': toSuper('3'), // for window resizing with rectangle
@@ -6,26 +6,67 @@ const semicolonKeyMappings = {
     'a': aWithDots(),
     'd': toKey('grave_accent_and_tilde', ['command', 'shift']), // prev window in app group
     'f': toKey('grave_accent_and_tilde', ['command']), // next window in app group
-    'i': open('IntelliJ Idea Ultimate'), // (i)de
+    'i': open('IntelliJ Idea Ultimate', 'com.jetbrains.intellij'), // (i)de
     's': oWithDots(),
     't': open('wezterm'), // (t)erminal
     'v': open('obsidian'), // (v)ault
-    'w': open('Safari.app'), // (w)eb
+    'w': open('Safari.app', 'com.apple.Safari'), // (w)eb
 }
 
 const config = require('./base.karabiner.json')
 
 const profile = config.profiles[0]
 const rule = profile.complex_modifications.rules.find(r => r.description === 'Set semicolon key to act as a modifier')
-const manipulators = rule.manipulators
+const target = rule.manipulators
 
-Object.entries(semicolonKeyMappings).forEach(([keyCode, actions]) => {
-    manipulators.push(semicolonKeyManipulator(keyCode, actions))
+Object.entries(keyToManipulators).forEach(([fromKeyCode, getManipulators]) => {
+    const manipulators = getManipulators(fromKeyCode)
+    target.push(...manipulators)
 })
 
 require('fs').writeFileSync('karabiner.json', JSON.stringify(config, null, 4))
 
-function semicolonKeyManipulator(keyCode, actions) {
+function toKey(toKeyCode, modifiers) {
+    const actions = genKeyAction(toKeyCode, modifiers)
+    return (fromKeyCode) => [genManipulator(fromKeyCode, actions)]
+}
+
+function toSuper(keyCode) {
+    return toKey(keyCode, ['left_command', 'left_alt', 'left_control', 'left_shift'])
+}
+
+function open(appName, bundleIdentifier) {
+    const openAppAction = [
+        {
+            shell_command: `open -a '${appName}'`
+        }
+    ]
+    if (bundleIdentifier) {
+        // enable switching between app windows when app already open
+        const conditions = (type) => [{ type, bundle_identifiers: [bundleIdentifier] }]
+        return (fromKeyCode) => [
+            genManipulator(fromKeyCode, openAppAction, conditions('frontmost_application_unless')),
+            genManipulator(
+                fromKeyCode,
+                genKeyAction('grave_accent_and_tilde', ['command']),
+                conditions('frontmost_application_if')
+            ),
+        ]
+    }
+    return (fromKeyCode) => [genManipulator(fromKeyCode, openAppAction)]
+}
+
+function aWithDots() {
+    const actions = aWithDotsActions()
+    return (fromKeyCode) => [genManipulator(fromKeyCode, actions)]
+}
+
+function oWithDots() {
+    const actions = oWithDotsActions()
+    return (fromKeyCode) => [genManipulator(fromKeyCode, actions)]
+}
+
+function genManipulator(fromKeyCode, toActions, extraConditions = []) {
     return {
         type: 'basic',
         conditions: [
@@ -34,23 +75,15 @@ function semicolonKeyManipulator(keyCode, actions) {
                 name: 'semicolon_modifier_active',
                 value: 1
             }
-        ],
+        ].concat(extraConditions),
         from: {
-            key_code: keyCode
+            key_code: fromKeyCode
         },
-        to: actions
+        to: toActions
     }
 }
 
-function open(appName) {
-    return [
-        {
-            shell_command: `open -a '${appName}'`
-        }
-    ]
-}
-
-function toKey(keyCode, modifiers = []) {
+function genKeyAction(keyCode, modifiers = []) {
     return [
         {
             key_code: keyCode,
@@ -59,11 +92,7 @@ function toKey(keyCode, modifiers = []) {
     ]
 }
 
-function toSuper(keyCode) {
-    return toKey(keyCode, ['left_command', 'left_alt', 'left_control', 'left_shift'])
-}
-
-function aWithDots() {
+function aWithDotsActions() {
     return [
         {key_code: "keypad_plus", modifiers: ["left_alt"]},
         {key_code: "0", modifiers: ["left_alt"]},
@@ -73,7 +102,7 @@ function aWithDots() {
     ]
 }
 
-function oWithDots() {
+function oWithDotsActions() {
     return [
         {key_code: "keypad_plus", modifiers: ["left_alt"]},
         {key_code: "0", modifiers: ["left_alt"]},
